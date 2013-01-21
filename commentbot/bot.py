9560fd0ce0d2e5e28b973ba6227a5f51e6e9f9d4
nat <natman97@when.com>
2013-01-20 22:02:13 -0800

    Initial commit.

---
+++ commentbot/bot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.3 -O
+
+# The most common words bot. By drkabob.
+# Finds the top most common words in a reddit comment thread
+# Copyright 2013 Nathan Hakkakzadeh. All rights reserved.
+import praw, sys
+
+# Before we do anything, we need to grab a list of the most common words to ignore
+file = open("commonwords.txt")
+commonwords = []
+for line in file:
+	commonwords.append(line.lower().rstrip())
+
+r = praw.Reddit(user_agent="PRAW Most Common Word Bot. By /u/drkabob")
+# Login would go here if it is necessary.
+
+# Get the submission...
+submission = r.get_submission(sys.argv[1])
+
+# We need to go deeper...
+submission.replace_more_comments()
+comments = praw.helpers.flatten_tree(submission.comments)
+
+wordcount = {}
+
+# Loop through the comments, split it up into words and count them up
+for comment in comments:
+	
+	words = comment.body.split()
+	
+	for word in words:
+		word = word.lower()
+		word = ''.join(e for e in word if e.isalnum())
+		if not word in commonwords:
+			if word in wordcount:
+				wordcount[word] += 1
+			else:
+				wordcount[word] = 1
+
+# Sort the list...
+winners = sorted(wordcount, key=wordcount.get, reverse=True)
+
+# Print the top 5 words
+for i in range(5):
+	print(winners[i] + " - " + str(wordcount[winners[i]]))
---
+++ commentbot/reddit_mcw.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3.3 -O
+
+# The most common words bot. By /u/drkabob
+# Finds the top most common words in a reddit comment thread
+# Copyright 2013 Nathan Hakkakzadeh. All rights reserved.
+import praw, sys, collections
+
+from bottle import get, request, run
+
+# Before we do anything, we need to grab a list of the most common words to ignore
+file = open("commonwords.txt")
+commonwords = []
+for line in file:
+	commonwords.append(line.lower().rstrip())
+	
+r = praw.Reddit(user_agent="PRAW Most Common Word Bot. By /u/drkabob")
+# Login would go here if it is necessary.
+
+
+# Function that actually does the dirty work
+# Returns an ordered dictionary with the words in order of how popular
+# link: Comment thread we want to parse
+# top: How many of the top words do we want to get (Default: 5)
+# WARNING: This function is SLOOOOOOW and will block the entire script.
+# You should REALLY run this in a thread.
+def get_top_words(link, top=5):
+	# Get the submission...
+	submission = r.get_submission(link)
+	
+	# We need to go deeper...
+	submission.replace_more_comments()
+	comments = praw.helpers.flatten_tree(submission.comments)
+	
+	wordcount = {}
+	
+	# Loop through the comments, split it up into words and count them up
+	for comment in comments:
+		
+		words = comment.body.split()
+		
+		for word in words:
+			word = word.lower()
+			word = ''.join(e for e in word if e.isalnum())
+			if not word in commonwords:
+				if word in wordcount:
+					wordcount[word] += 1
+				else:
+					wordcount[word] = 1
+	
+	# Sort the list...
+	winners = sorted(wordcount, key=wordcount.get, reverse=True)
+	
+	# Build the dictionary we want to return...
+	toreturn = collections.OrderedDict()
+	for i in range(top):
+		toreturn[winners[i]] = wordcount[winners[i]]
+		
+	return toreturn
+
+@get("/mcw")
+def mcw():
+	link = request.query.link
+	
+	if link == "":
+		return """<title>Most Common Words for Reddit comment threads</title>
+				<h3>Most Common Words for Reddit comment threads</h3>
+				<form name="input" action="mcw" method="get">
+					Comment Thread Link: <input type="text" size="50" name="link">
+					<br />
+					Top <select name="top">
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5" selected>5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+						</select> words.
+						<br /> <br />
+						<input type="submit" value="Calculate">
+				</form>
+				
+				Warning, will take a long time to load. Do NOT try to refresh page.
+				A tool by <a href="http://www.reddit.com/user/drkabob">Nathan.</a>"""
+	else:
+		results = get_top_words(link, top=int(request.query.top))
+		
+		toreturn = """<title>Most Common Words for Reddit comment threads</title>
+					<h3>Most common words for comment thread given</h3>
+					"""
+		i = 1
+		for result in results:
+			toreturn = toreturn + str(i) + ") " + result + " - " + str(results[result])+ "<br />"
+			i += 1
+			
+		toreturn = toreturn + """<br /><br /><FORM METHOD="LINK" ACTION="mcw"> <INPUT TYPE="submit" VALUE="Back"> </FORM>"""
+		return toreturn
+		
+# Run the web server!
+run(server="twisted")
